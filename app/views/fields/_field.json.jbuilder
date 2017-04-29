0ba9fd884e3b21ebd93676c32a336b15810d32ea
Martín De la Fuente <mdelafuente1@uc.cl>
2017-04-29 04:21:11 -0300

    Removed Field views and refactored routes

--- app/views/fields/_field.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! field, :id, :gee_id, :name, :ttype, :min_value, :max_value, :created_at, :updated_at
-json.url field_url(field, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :gees
-  resources :fields
-  resources :bets
-  resources :categories
+
+  root to: 'gees#index'
+
+  resources :gees do
+    resources :bets, only: [:new, :create, :index, :show]
+  end
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories, only: [:new, :create, :destroy]
 end
