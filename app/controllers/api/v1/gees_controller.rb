b0f0251d11154b34fa0e7176234462cd128df55c
Andres Espinosa <andresespinosapc@gmail.com>
2017-06-26 23:57:55 -0400

    Fixes

--- app/controllers/api/v1/gees_controller.rb
+++ app/controllers/api/v1/gees_controller.rb
@@ -12,30 +12,31 @@ module Api::V1
 
     def create
       new_params = gee_params
-      new_params[:user_id] = current_user[:id]
+      new_params[:user_id] = @current_user[:id]
       @gee = Gee.new(new_params)
       unless @gee.is_public
-        @gee.users << current_user
+        @gee.users << @current_user
       end
       @fields = []
 
-      params[:fields].each do |field|
-        if field[:type] == 'number'
+      Rails.logger.debug(params.inspect)
+      params[:fields].each do |field_param|
+        if field_param[:type] == 'number'
           field = Field.new(
             gee: @gee.id,
-            name: field[:name],
-            ttype: field[:type].capitalize,
-            min_value: field[:min_value].to_f,
-            max_value: field[:max_value].to_f)
-        elsif field[:type] == 'alternatives'
+            name: field_param[:name],
+            ttype: field_param[:type].capitalize,
+            min_value: field_param[:min_value].to_f,
+            max_value: field_param[:max_value].to_f)
+        elsif field_param[:type] == 'alternatives'
           field = Field.new(
             gee: @gee.id,
-            name: field[:name],
-            ttype: field[:type].capitalize,
+            name: field_param[:name],
+            ttype: field_param[:type].capitalize,
             min_value: nil,
             max_value: nil)
           @alternatives = []
-          field[:alternatives].each do |alternative|
+          field_param[:alternatives].each do |alternative|
             alternative = Alternative.new(
               field: field.id,
               value: alternative)
