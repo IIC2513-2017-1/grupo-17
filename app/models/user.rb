420b8d2d0dfe0538d60c17179e6be91cf65028aa
Andres Espinosa <andresespinosapc@gmail.com>
2017-07-04 15:44:27 -0400

    Little fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   has_attached_file :avatar,
     :styles => { :medium => "300x300#", :thumb => "100x100#" },
     :default_url => "/images/:style/missing.png",
-    :path => "/tmp/attachments/:attachment/:id/:style/:filename"
+    :path => "/tmp/attachments/:attachment/:id/:style/:filename",
     :url => "/tmp/attachments/:attachment/:id/:style/:filename"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
